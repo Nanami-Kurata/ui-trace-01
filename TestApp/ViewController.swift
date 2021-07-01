e6201611db23780a9cc8fdfae8030d86bde6d878
Nanami Kurata <krtnnm20011031@gmail.com>
2021-07-01 14:37:02 +0900

    first commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by 倉田七海 on 2021/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var label2: UILabel!
+}
+
